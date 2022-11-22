05b65d83279f60837c38e7d1bbd79ea9ac592a9c
Anton Asipenka <anton.acipenka@gmail.com>
2022-11-22 21:43:25 +0300

    Fixed constants name

--- src/main/java/com/laborete/LaboreteAPI/services/UsersService.java
+++ src/main/java/com/laborete/LaboreteAPI/services/UsersService.java
@@ -12,12 +12,12 @@ import java.util.UUID;
 
 @Service
 public class UsersService {
-    public static final String userNotFound = "User did not find with id:";
-    public static final String firstNameIsRequired = "The user name cannot be empty";
-    public static final String secondNameIsRequired = "The secondName cannot be empty";
-    public static final String positionIsRequired = "The position cannot be empty";
-    public static final String locationIsRequired = "The location cannot be empty";
-    public static final String uuidIsRequired = "The UUID cannot be empty";
+    private static final String USER_NOT_FOUND = "User did not find with id:";
+    private static final String FIRST_NAME_IS_REQUIRED = "The user name cannot be empty";
+    private static final String SECOND_NAME_IS_REQUIRED = "The secondName cannot be empty";
+    private static final String POSITION_IS_REQUIRED = "The position cannot be empty";
+    private static final String LOCATION_IS_REQUIRED = "The location cannot be empty";
+    private static final String UUID_IS_REQUIRED = "The UUID cannot be empty";
     private final UsersRepository usersRepository;
 
     public UsersService(UsersRepository usersRepository) {
@@ -26,27 +26,27 @@ public class UsersService {
 
     public Users getUserById(UUID uuid) {
         if (uuid == null) {
-            throw new ResourceBadRequestException(uuidIsRequired);
+            throw new ResourceBadRequestException(UUID_IS_REQUIRED);
         }
         Users user = this.usersRepository.getUsersById(uuid);
         if (user == null) {
-            throw new ResourceNotFoundException(userNotFound + uuid);
+            throw new ResourceNotFoundException(USER_NOT_FOUND + uuid);
         }
         return user;
     }
 
     public Users createUser(Users user) {
         if (user.getFirstName() == null || user.getFirstName().isEmpty()) {
-            throw new ResourceBadRequestException(firstNameIsRequired);
+            throw new ResourceBadRequestException(FIRST_NAME_IS_REQUIRED);
         }
         if (user.getSecondName() == null || user.getSecondName().isEmpty()) {
-            throw new ResourceBadRequestException(secondNameIsRequired);
+            throw new ResourceBadRequestException(SECOND_NAME_IS_REQUIRED);
         }
         if (user.getLocation() == null || user.getLocation().isEmpty()) {
-            throw new ResourceBadRequestException(locationIsRequired);
+            throw new ResourceBadRequestException(LOCATION_IS_REQUIRED);
         }
         if (user.getPosition() == null || user.getPosition().isEmpty()) {
-            throw new ResourceBadRequestException(positionIsRequired);
+            throw new ResourceBadRequestException(POSITION_IS_REQUIRED);
         }
 
         return this.usersRepository.save(user);
