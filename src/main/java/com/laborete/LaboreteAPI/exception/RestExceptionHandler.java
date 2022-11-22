63092a0e1879406d496c66557062c0f8fa787075
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2022-11-23 00:19:51 +0200

    RETE-3: Refactor

--- src/main/java/com/laborete/LaboreteAPI/exception/RestExceptionHandler.java
+++ src/main/java/com/laborete/LaboreteAPI/exception/RestExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.laborete.LaboreteAPI.exception;
 
-import com.laborete.LaboreteAPI.modal.ErrorResponse;
+import com.laborete.LaboreteAPI.models.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
