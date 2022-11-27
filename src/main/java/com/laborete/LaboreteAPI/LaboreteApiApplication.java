6dc9835c5e88a833b7017310e98da8364834b0d8
Aasipenka <anton.acipenka@gmail.com>
2022-11-27 23:40:31 +0300

    RETE-7 Disabled cors blocking for port 4200

--- src/main/java/com/laborete/LaboreteAPI/LaboreteApiApplication.java
+++ src/main/java/com/laborete/LaboreteAPI/LaboreteApiApplication.java
@@ -2,6 +2,9 @@ package com.laborete.LaboreteAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LaboreteApiApplication {
@@ -9,5 +12,13 @@ public class LaboreteApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LaboreteApiApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
