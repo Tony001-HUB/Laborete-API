bec5daa297ca81b7fc707e73bc41ffd5f80bcca9
Anton Asipenka <anton.acipenka@gmail.com>
2022-12-30 10:50:09 +0300

    Moved to java 17 && maven

---
+++ src/main/java/com/laborete/LaboreteAPI/LaboreteApiApplication.java
@@ -0,0 +1,13 @@
+package com.laborete.LaboreteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboreteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laborete/LaboreteAPI/LaboreteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laborete.LaboreteAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboreteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
