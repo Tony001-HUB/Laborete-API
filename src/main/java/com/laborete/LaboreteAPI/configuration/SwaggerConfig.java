9de9950c12264899a5dd1d836dda03940172af84
Anton Asipenka <aasipenka@elinext.com>
2023-01-04 20:03:49 +0300

    Added SwaggerFox

---
+++ src/main/java/com/laborete/LaboreteAPI/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.laborete.LaboreteAPI.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Laborete",
+                "APIs for Laborete app.",
+                "1.0",
+                "...",
+                new Contact("Laborete", "www.org.com", "test@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
