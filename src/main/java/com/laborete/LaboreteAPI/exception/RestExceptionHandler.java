9ef9914c62fa7bf8803ad1bfbea0fb94f6253205
seanans <maks1998ivan@gmail.com>
2023-02-10 14:38:57 +0200

    RETE-18: Codegen

--- src/main/java/com/laborete/LaboreteAPI/exception/RestExceptionHandler.java
+++ src/main/java/com/laborete/LaboreteAPI/exception/RestExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import springfox.documentation.annotations.ApiIgnore;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
